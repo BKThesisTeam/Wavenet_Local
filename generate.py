968d6bb498ceca0ed9399ab2f7d2a91c6b58235e
Nhu Dinh Toan <toanbk21096@gmail.com>
2018-03-14 01:39:26 +0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -285,7 +285,7 @@ def main():
 
     # Save the result as an audio summary.
     datestring = str(datetime.now()).replace(' ', 'T')
-    writer = tf.train.SummaryWriter(logdir)
+    writer = tf.summary.FileWriter(logdir)
     tf.audio_summary('generated', decode, wavenet_params['sample_rate'])
     summaries = tf.merge_all_summaries()
     summary_out = sess.run(summaries,
